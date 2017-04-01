0ff8fcc999a6baebb771c33281912e912550fa63
Nikita Maslov <maslovn@gmail.com>
2017-04-01 23:24:47 +0300

    add solution and mugen projects

---
+++ Mugen.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MugenMvvmToolkit.Interfaces.Models;
+using MugenMvvmToolkit.ViewModels;
+
+namespace Mugen.Core.ViewModels
+{
+    public class MainViewModel : CloseableViewModel, IHasDisplayName
+    {
+        public string DisplayName => "Mugen main view model";
+    }
+}
---
+++ Mugen.View/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Mugen.Core.ViewModels;
+using MugenMvvmToolkit;
+using MugenMvvmToolkit.WPF.Infrastructure;
+
+namespace Mugen.View
+{
+    public partial class App : Application
+    {
+        #region Constructors
+
+        public App()
+        {
+            new Bootstrapper<MainViewModel>(this, new AutofacContainer());
+        }
+
+        #endregion
+    }
+}
