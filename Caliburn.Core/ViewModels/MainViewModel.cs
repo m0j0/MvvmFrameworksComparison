ca4b03421730a64b14d8788b2ea08d2edaf489fa
Nikita Maslov <maslovn@gmail.com>
2017-04-02 22:45:09 +0300

    caliburn: initial commit

---
+++ Caliburn.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Caliburn.Micro;
+
+namespace Caliburn.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        public override string DisplayName => "Main view model";
+    }
+}
---
+++ Caliburn.View.Wpf/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Caliburn
+{
+    public partial class App : Application
+    {
+    }
+}
