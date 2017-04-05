6283994dc56c96e93f838ae9335ed186718aa9d6
Nikita Maslov <maslovn@gmail.com>
2017-04-05 17:07:39 +0300

    prism: add main vm initialization

--- Prism.View.Wpf/Bootstrapper.cs
+++ Prism.View.Wpf/Bootstrapper.cs
@@ -1,12 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Autofac;
 using Prism.Autofac;
+using Prism.Mvvm;
 using Prism.View.Wpf.Views;
+using Prism.ViewModels;
 
 namespace Prism.View.Wpf
 {
     public class Bootstrapper : AutofacBootstrapper
     {
+        #region Methods
+
         protected override DependencyObject CreateShell()
         {
             return Container.Resolve<MainView>();
@@ -16,5 +21,26 @@ namespace Prism.View.Wpf
         {
             Application.Current.MainWindow.Show();
         }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver(
+                viewType =>
+                {
+                    if (viewType == typeof(MainView))
+                    {
+                        return typeof(MainViewModel);
+                    }
+                    if (viewType == typeof(ChildView))
+                    {
+                        return typeof(ChildViewModel);
+                    }
+                    throw new NotSupportedException();
+                });
+        }
+
+        #endregion
     }
 }
