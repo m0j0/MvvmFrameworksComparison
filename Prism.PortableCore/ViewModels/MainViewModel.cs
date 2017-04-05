39ba12c5d9d83cdd16853db0192ddaacc7c7f711
Nikita Maslov <maslovn@gmail.com>
2017-04-05 17:13:32 +0300

    prism: work on main vm

--- Prism.PortableCore/ViewModels/MainViewModel.cs
+++ Prism.PortableCore/ViewModels/MainViewModel.cs
@@ -1,9 +1,80 @@
-﻿using Prism.Mvvm;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace Prism.ViewModels
 {
     public class MainViewModel : BindableBase
     {
+        #region Fields
+        
+        private bool _canOpenChildViewModel;
+        private string _parameter;
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            CanOpenChildViewModel = true;
+            OpenChildViewModelCommand = new DelegateCommand(OpenChildViewModel, CanExecuteOpenChildViewModel).ObservesProperty(() => CanOpenChildViewModel);
+        }
+
+        #endregion
+
+        #region Properties
+
         public string DisplayName => "Main view model";
+
+        public bool CanOpenChildViewModel
+        {
+            get { return _canOpenChildViewModel; }
+            set
+            {
+                if (value == _canOpenChildViewModel)
+                {
+                    return;
+                }
+
+                _canOpenChildViewModel = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Parameter
+        {
+            get { return _parameter; }
+            set
+            {
+                if (value == _parameter)
+                {
+                    return;
+                }
+
+                _parameter = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenChildViewModelCommand { get; }
+
+        private void OpenChildViewModel()
+        {
+
+
+        }
+
+        private bool CanExecuteOpenChildViewModel()
+        {
+            return CanOpenChildViewModel;
+        }
+
+        #endregion
     }
 }
