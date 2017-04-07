5685b5de4e5e9479b7789f4138f3f9001a21ff6f
Nikita Maslov <maslovn@gmail.com>
2017-04-07 22:51:26 +0300

    mugen: add winforms project

---
+++ Mugen.View.WinForms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using Mugen.ViewModels;
+using MugenMvvmToolkit;
+using MugenMvvmToolkit.WinForms.Infrastructure;
+
+namespace Mugen
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var bootstrapper = new Bootstrapper<MainViewModel>(new AutofacContainer());
+            bootstrapper.Start();
+        }
+    }
+}
---
+++ Mugen.View.WinForms/Views/MainView.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+using Mugen.ViewModels;
+using MugenMvvmToolkit.Binding;
+using MugenMvvmToolkit.Binding.Builders;
+
+namespace Mugen.Views
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            using (var set = new BindingSet<MainViewModel>())
+            {
+                set.Bind(this, nameof(Text))
+                    .To(nameof(MainViewModel.DisplayName));
+                set.Bind(checkBoxCanOpenChildWindow, nameof(checkBoxCanOpenChildWindow.Checked))
+                    .To(nameof(MainViewModel.CanOpenChildViewModel))
+                    .TwoWay();
+                set.Bind(textBoxParameter, nameof(checkBoxCanOpenChildWindow.Text))
+                    .To(nameof(MainViewModel.Parameter))
+                    .TwoWay();
+                set.Bind(buttonOpenChildWindow, nameof(buttonOpenChildWindow.Click))
+                    .To(nameof(MainViewModel.OpenChildViewModelCommand));
+            }
+        }
+    }
+}
