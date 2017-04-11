4622db7b9235b5e6be982950dcf8daf549680b79
Nikita Maslov <maslovn@gmail.com>
2017-04-11 15:48:31 +0300

    light: move to csproj

--- Light.PortableCore/Properties/AssemblyInfo.cs
+++ Light.PortableCore/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Light.Core")]
+[assembly: AssemblyTitle("Light.PortableCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Light.Core")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("Light.PortableCore")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
