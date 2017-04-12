fffc79666415e1128d1fb63f918f3b5fe29a08d4
Nikita Maslov <maslovn@gmail.com>
2017-04-13 01:21:52 +0300

    mugen: minor winforms changes

--- Mugen.View.WinForms/Views/CompositeView.Designer.cs
+++ Mugen.View.WinForms/Views/CompositeView.Designer.cs
@@ -28,26 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.compositeNestedView3 = new Mugen.Views.CompositeNestedView();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.nestedTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tableLayoutPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.compositeNestedView3, 0, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(333, 238);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.Controls.Add(this.compositeNestedView3, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.buttonClose, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.nestedTableLayoutPanel, 0, 1);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(333, 238);
+            this.tableLayoutPanel.TabIndex = 0;
             // 
             // compositeNestedView3
             // 
@@ -56,21 +60,45 @@
             this.compositeNestedView3.Size = new System.Drawing.Size(153, 32);
             this.compositeNestedView3.TabIndex = 0;
             // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(3, 3);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // nestedTableLayoutPanel
+            // 
+            this.nestedTableLayoutPanel.ColumnCount = 1;
+            this.nestedTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.nestedTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.nestedTableLayoutPanel.Location = new System.Drawing.Point(3, 82);
+            this.nestedTableLayoutPanel.Name = "nestedTableLayoutPanel";
+            this.nestedTableLayoutPanel.RowCount = 1;
+            this.nestedTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.nestedTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.nestedTableLayoutPanel.Size = new System.Drawing.Size(200, 73);
+            this.nestedTableLayoutPanel.TabIndex = 2;
+            // 
             // CompositeView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Name = "CompositeView";
             this.Size = new System.Drawing.Size(333, 238);
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private CompositeNestedView compositeNestedView3;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TableLayoutPanel nestedTableLayoutPanel;
     }
 }
--- Mugen.View.WinForms/Views/CompositeView.cs
+++ Mugen.View.WinForms/Views/CompositeView.cs
@@ -3,6 +3,7 @@ using Mugen.ViewModels;
 using MugenMvvmToolkit.Binding;
 using MugenMvvmToolkit.Binding.Builders;
 using MugenMvvmToolkit.WinForms.Binding;
+using MugenMvvmToolkit.WinForms.Binding.Infrastructure;
 
 namespace Mugen.Views
 {
@@ -11,12 +12,38 @@ namespace Mugen.Views
         public CompositeView()
         {
             InitializeComponent();
-
+            
             using (var set = new BindingSet<CompositeViewModel>())
             {
+                set.Bind(buttonClose)
+                    .To(() => (vm, ctx) => vm.CloseCommand);
+
+                nestedTableLayoutPanel.SetBindingMemberValue(AttachedMembers.Control.ContentViewManager, new ContentViewManager());
+                set.Bind(nestedTableLayoutPanel, AttachedMemberConstants.Content)
+                    .To(() => (vm, ctx) => vm.FirstNestedViewModel);
+
                 set.Bind(compositeNestedView3, AttachedMembers.Object.DataContext)
                     .To(() => (vm, ctx) => vm.ThirdNestedViewModel);
             }
         }
+    }
+
+    public class ContentViewManager : ContentViewManagerBase<Control, Control>
+    {
+        #region Overrides of ContentViewManagerBase<Control,Control>
+
+        protected override void SetContent(Control view, Control content)
+        {
+            if (content == null)
+            {
+                view.Controls.Clear();
+            }
+            else
+            {
+                view.Controls.Add(content);
+            }
+        }
+
+        #endregion
     }
 }
